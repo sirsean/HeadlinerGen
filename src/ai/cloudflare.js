e4cafcc4042c43c26d3d4a1015058393b5fc9e68
Sean Schulte <sirsean@gmail.com>
2024-10-10 14:34:58 +0000

    refactor the AI generators, and split the prompts into a summary generator and an image prompt generator, so we can keep the art improvement while sticking to recording the news a bit better

--- src/ai/cloudflare.js
+++ src/ai/cloudflare.js
@@ -1,106 +1,43 @@
-//import fetch from 'node-fetch';
+export class CloudflareAI {
+  constructor(model) {
+    this.model = model;
+  }
 
-export class ImageGenerator {
   async generate(prompt) {
-    return fetch(this.url, {
+    return fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/${this.model}`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.CLOUDFLARE_AUTH_TOKEN}`,
       },
-      body: JSON.stringify(this.requestBody(prompt)),
+      body: JSON.stringify(prompt),
     }).then(res => {
-      console.log(`imgen status: ${res.status}: ${res.statusText}`);
+      console.log(`AI status (${this.model}): ${res.status}: ${res.statusText}`);
       if (!res.ok) {
         throw new Error(res.statusText);
       }
       return res;
-    }).then(res => res.body);
-  }
-
-  get url() {
-      throw new Error('Not implemented: url');
-  }
-
-  requestBody(prompt) {
-      throw new Error('Not implemented: requestBody');
+    })
   }
 }
 
-export class StableDiffusionBaseImageGenerator extends ImageGenerator {
-  get url() {
-      return `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0`;
-  }
-
-  requestBody(prompt) {
-      return {
-          prompt,
-      };
-  }
-}
-
-export class StableDiffusionLightningImageGenerator extends ImageGenerator {
-  get url() {
-    return `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/bytedance/stable-diffusion-xl-lightning`;
-  }
-
-  requestBody(prompt) {
-    return {
-      prompt,
-    };
-  }
-}
-
-export class BlurbGenerator {
-  constructor(id) {
-    this.id = id;
-  }
-
-  async generate() {
-    return fetch(
-      this.url,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
-        },
-        body: JSON.stringify(this.requestBody()),
-      },
-    )
-      .then((res) => {
-        console.log(`textgen status: ${res.status}: ${res.statusText}`);
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res;
-      })
-      .then((res) => res.json())
-      .then((res) => {
-        if (!res.success) {
-          throw new Error(res.errors);
-        }
-        return res.result.response;
-      });
-  }
-
-  get url() {
-    return `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct`;
-  }
-
-  requestBody() {
-    return {
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are writing one-sentence blurbs that describe the biography of an individual eyeball. These eyeballs are mystical money from the future, from a universe infused with magic unicorns. Be imaginative and quirky. None of the eyeballs have names or numbers. Do not mention that you are a Large Language Machine. There is always a blurb. Please do not fail to generate one, and do not explain that you couldn't succeed.",
-        },
-        {
-          role: "user",
-          content: `Tell me about an eyeball. Its ID number is ${this.id}.`,
-        },
-      ],
+export class TextCloudflareAI extends CloudflareAI {
+  async generate({ system, user }) {
+    const prompt = { messages: [] };
+    if (system) {
+      system.forEach(message => prompt.messages.push({ role: 'system', content: message }));
     }
+    if (user) {
+      user.forEach(message => prompt.messages.push({ role: 'user', content: message }));
+    }
+    return super.generate(prompt)
+      .then(res => res.json())
+      .then(res => res.result.response);
+  }
+}
+
+export class ImageCloudflareAI extends CloudflareAI {
+  async generate(prompt) {
+    return super.generate({ prompt })
+      .then(res => res.body);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
 import Parser from 'rss-parser';
-import { StableDiffusionLightningImageGenerator } from './ai/cloudflare.js';
+import { TextCloudflareAI, ImageCloudflareAI } from './ai/cloudflare.js';
 import { writeFile } from 'fs/promises';
 import { downloadAll, uploadAll, uploadCurrent, uploadImage, uploadMetadata, imgToThumbnail } from './data/r2.js';
 import * as uuid from 'uuid';
 
-async function summarize(text) {
-  return fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/meta/llama-3.2-3b-instruct`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${process.env.CLOUDFLARE_AUTH_TOKEN}`,
-    },
-    body: JSON.stringify({
-      messages: [
-        {
-          role: 'system',
-          content: 'You are the editor of a news desk, responsible for taking the stories of the day and summarizing them into a single written paragraph. You must decide which is the most important over-arching topic of the day, from the provided news snippets. Once you decide the single best topic, describe it visually, from the perspective of an artist at the scene. Return only this summary, and no prelude.',
-        },
-        {
-          role: 'user',
-          content: text,
-        },
-      ],
-    }),
-  })
-    .then(r => r.json())
-    .then(r => r.result.response);
+async function summarizeTopStory(text) {
+  const ai = new TextCloudflareAI('meta/llama-3.2-3b-instruct');
+  return ai.generate({
+    system: ['You are the editor of a news desk, responsible for taking the stories of the day and summarizing them into a single written paragraph. You must decide which is the most important over-arching topic of the day, from the provided news snippets. Return only this summary, and no prelude.'],
+    user: [text],
+  });
+}
+
+async function createImagePrompt(summary) {
+  const ai = new TextCloudflareAI('meta/llama-3.2-3b-instruct');
+  return ai.generate({
+    system: ['You are the leader of the art desk at a news magazine. You are given a news story, and you must create a compelling and iconic image representing the given story. Describe this story visually, from the perspective of an artist at the scene. Return only this image description, and no prelude.'],
+    user: [summary],
+  });
 }
 
 async function fetchNews() {
@@ -34,7 +27,7 @@ async function fetchNews() {
 }
 
 async function summarizeNews(items) {
-  return summarize(
+  return summarizeTopStory(
     items
       .map(({ title, content }) => {
         return `${title}: ${content}`;
@@ -48,16 +41,19 @@ async function main() {
   
   const id = uuid.v4();
   const summary = await fetchNews().then(summarizeNews);
-  console.log(summary);
-  const img = await new StableDiffusionLightningImageGenerator().generate(`Generate an illustration suitable to be placed in a news briefing, abstract, with no text or maps or charts; the image should be an artist's depiction of what the event looks like at the scene: ${summary}`);
-  //await writeFile('./tmp/generated_image.png', img);
-
+  const prompt = await createImagePrompt(summary);
+  const img = await new ImageCloudflareAI('bytedance/stable-diffusion-xl-lightning')
+    .generate(`Generate an illustration suitable to be placed in a news briefing, abstract, with no text or maps or charts; the image should be an artist's depiction of what the event looks like at the scene: ${prompt}`);
+  
   const metadata = {
     id,
     summary,
+    prompt,
     at: new Date(),
   };
 
+  //await writeFile('./tmp_image.png', img);
+  
   await uploadImage(id, img);
   await imgToThumbnail(id);
   await uploadMetadata(id, metadata);
@@ -66,7 +62,7 @@ async function main() {
   await uploadAll(all);
 
   await uploadCurrent(metadata);
-
+  
   console.log(metadata);
 }
 
